9009c2f016855ed130fbf0df8db59329257d9ef3
ezl <ericzliu@gmail.com>
2013-12-08 11:06:47 -0600

    Style inputs using bootstrap

--- project/annotate/forms.py
+++ project/annotate/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 
 class AnnotatedDocumentCreationForm(forms.Form):
-    title = forms.CharField(required=False)
-    slug = forms.CharField(required=False)
-    content = forms.CharField(widget=forms.Textarea)
+    title = forms.CharField(required=False,
+            widget=forms.TextInput(attrs={'class':'form-control'}))
+    slug = forms.CharField(required=False,
+            widget=forms.TextInput(attrs={'class':'form-control'}))
+    content = forms.CharField(
+            widget=forms.Textarea(attrs={'class':'form-control'}))
