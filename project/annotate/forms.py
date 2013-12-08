21b8fa663355a6f1a2f3b53a27e8cea85a463706
ezl <ericzliu@gmail.com>
2013-12-08 12:49:12 -0600

    remove unused slug form field

--- project/annotate/forms.py
+++ project/annotate/forms.py
@@ -3,7 +3,7 @@ from django import forms
 class AnnotatedDocumentCreationForm(forms.Form):
     title = forms.CharField(required=False,
             widget=forms.TextInput(attrs={'class':'form-control'}))
-    slug = forms.CharField(required=False,
-            widget=forms.TextInput(attrs={'class':'form-control'}))
+    # slug = forms.CharField(required=False,
+    #         widget=forms.TextInput(attrs={'class':'form-control'}))
     content = forms.CharField(
             widget=forms.Textarea(attrs={'class':'form-control'}))
